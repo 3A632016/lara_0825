21bcf141be2f51816d8c963893925bff8c747c53
3A632016 <winnie771737@gmail.com>
2019-11-05 11:35:45 +0800

    增加兩個欄位

--- database/migrations/2019_11_05_030125_create_posts_table.php
+++ database/migrations/2019_11_05_030125_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            
+
             $table->string('title');
             $table->text('content');
         });
